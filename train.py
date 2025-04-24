530d1da84d351f62e7570e0128ae4b23a3f810eb
PeDiot <p.emmanuel.diot@gmail.com>
2025-04-24 12:44:38 +0200

    Add functions for creating and deploying AutoML datasets and models in Vertex AI, including logging and handling of credentials. Refactor main function to utilize these new methods.

--- train.py
+++ train.py
@@ -1,4 +1,7 @@
-from src import crystalvalue
+import logging
+from typing import Any, Optional
+
+from google.cloud import aiplatform, bigquery
 from google.oauth2 import service_account
 
 
@@ -17,41 +20,138 @@ IGNORE_COLUMNS = ["order_number", "days_to_next_order", "shipping_address_dept"]
 LOOKBACK_DAYS = 0
 LOOKAHEAD_DAYS = 365
 
+_NON_FEATURES = [
+    "customer_id",
+    "window_date",
+    "lookback_start",
+    "lookahead_start",
+    "lookahead_stop",
+    "future_value",
+    "future_value_classification",
+    "predefined_split_column",
+]
 
-def main(feature_engineering: bool = False):
+
+def create_automl_dataset(
+    project_id: str,
+    dataset_id: str,
+    table_name: str = "training_data",
+    dataset_display_name: str = "crystalvalue_dataset",
+    location: str = "europe-west4",
+    credentials: Optional[Any] = None
+) -> aiplatform.datasets.tabular_dataset.TabularDataset:
+    logging.info(
+        "Creating Vertex AI Dataset with display name %r", dataset_display_name
+    )
+    bigquery_uri = f"bq://{project_id}.{dataset_id}.{table_name}"
+
+    aiplatform.init(
+        project=project_id, 
+        location=location, 
+        credentials=credentials
+    )
+    dataset = aiplatform.TabularDataset.create(
+        display_name=dataset_display_name, bq_source=bigquery_uri
+    )
+
+    dataset.wait()
+    return dataset
+
+
+def train_automl_model(
+    project_id: str,
+    aiplatform_dataset: aiplatform.TabularDataset,
+    model_display_name: str = "crystalvalue_model",
+    predefined_split_column_name: str = "predefined_split_column",
+    target_column: str = "future_value",
+    optimization_objective: str = "minimize-rmse",
+    optimization_prediction_type: str = "regression",
+    budget_milli_node_hours: int = 1000,
+    location: str = "europe-west4",
+    credentials: Optional[Any] = None
+) -> aiplatform.models.Model:
+    logging.info(
+        "Creating Vertex AI AutoML model with display name %r", model_display_name
+    )
+
+    transformations = [
+        {"auto": {"column_name": f"{feature}"}}
+        for feature in aiplatform_dataset.column_names
+        if feature not in _NON_FEATURES
+    ]
+
+    aiplatform.init(
+        project=project_id, 
+        location=location, 
+        credentials=credentials
+    )
+
+    job = aiplatform.AutoMLTabularTrainingJob(
+        display_name=model_display_name,
+        optimization_prediction_type=optimization_prediction_type,
+        optimization_objective=optimization_objective,
+        column_transformations=transformations,
+    )
+
+    model = job.run(
+        dataset=aiplatform_dataset,
+        target_column=target_column,
+        budget_milli_node_hours=budget_milli_node_hours,
+        model_display_name=model_display_name,
+        predefined_split_column_name=predefined_split_column_name,
+    )
+
+    model.wait()
+    logging.info("Created AI Platform Model with display name %r", model.display_name)
+    return model
+
+
+def deploy_model(
+    bigquery_client: bigquery.Client,
+    model_id: str,
+    machine_type: str = "n1-standard-2",
+    location: str = "europe-west4",
+    credentials: Optional[Any] = None
+) -> aiplatform.Model:
+    aiplatform.init(project=bigquery_client.project, location=location, credentials=credentials)
+    model = aiplatform.Model(model_name=model_id)
+    model.deploy(machine_type=machine_type)
+    model.wait()
+    
+    logging.info("Deployed model with display name %r", model.display_name)
+
+    return model
+
+
+def main():
     credentials = service_account.Credentials.from_service_account_file(
         filename=CREDENTIALS_PATH
     )
 
-    pipeline = crystalvalue.CrystalValue(
+    bq_client = bigquery.Client(credentials=credentials)
+
+    dataset = create_automl_dataset(
         project_id=GCP_PROJECT_ID,
         dataset_id=GCP_DATASET_ID,
-        credentials=credentials,
-        customer_id_column=CUSTOMER_ID_COLUMN,
-        date_column=DATE_COLUMN,
-        value_column=VALUE_COLUMN,
-        days_lookback=LOOKBACK_DAYS,
-        days_lookahead=LOOKAHEAD_DAYS,
-        ignore_columns=IGNORE_COLUMNS,
+        table_name=GCP_TABLE_ID,
         location=GCP_LOCATION,
-        write_parameters=True,
+        credentials=credentials
     )
 
-    if feature_engineering:
-        pipeline.feature_engineer(
-            transaction_table_name=GCP_TABLE_ID,
-            query_type="train_query",
-        )
-        print("Feature engineering done")
-
-    pipeline.train_automl_model(
-        dataset_display_name=GCP_DATASET_ID,
-        model_display_name=GCP_DATASET_ID
+    model = train_automl_model(
+        project_id=GCP_PROJECT_ID,
+        aiplatform_dataset=dataset,
+        model_display_name="crystalvalue_model",
+        location=GCP_LOCATION,
+        credentials=credentials
     )
-    print("Training done")
 
-    pipeline.deploy_model()
-    print("Deploying done")
+    deploy_model(
+        bigquery_client=bq_client,
+        model_id=model.name,
+        location=GCP_LOCATION,
+        credentials=credentials
+    )
 
 
 if __name__ == "__main__":
